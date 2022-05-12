180c11d85e2e7c159427e71d1aa6b1aa7e8604e8
Victoria Solís <dual.victoria.solis@gmail.com>
2022-05-12 16:12:47 -0500

    Struct con llaves del diccionario

---
+++ Entidades/LlavesDiccionario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.Entidades
+{
+    public struct LlavesDiccionario
+    {
+    public const string Cursos = "Cursos";
+    public const string Alumnos = "Alumnos";
+    public const string Asignaturas = "Asignaturas";
+    public const string Escuela = "Escuela";
+    public const string Evaluaciones = "Evaluaciones";
+    }
+}
