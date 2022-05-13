e1e4c47e62a35e6932f68b6dcfe09251b32c884e
Victoria Solís <dual.victoria.solis@gmail.com>
2022-05-13 15:42:40 -0500

    Printer actualizado y algunas excepciones para ingresar notas dentro de program.cs

--- Program.cs
+++ Program.cs
@@ -31,13 +31,58 @@ namespace CoreEscuela
             var listaAsg = reporteador.GetListaAsignaturas();
             var listaPromXAsig = reporteador.GetPromeAlumnPorAsignatura();
 
-            foreach (var item in listaPromXAsig)
+            Printer.WriteTitle("Captura de una Evaluación por Consola");
+            var newEval = new Evaluación();
+            string nombre, notastring;
+            float nota;
+
+            WriteLine("Ingrese el nombre de la evaluación");
+            Printer.PresioneENTER();
+            nombre = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nombre))
             {
-                foreach (var alum in item.Value)
-                {
-                    
-                }
+                Printer.WriteTitle("El valor del nombre no puede ser vacio");
+                WriteLine("Saliendo del programa");
             }
+            else
+            {
+                newEval.Nombre = nombre.ToLower();
+                WriteLine("El nombre de la evaluacion ha sido ingresado correctamente");
+            }
+
+
+            WriteLine("Ingrese la nota de la evaluación");
+            Printer.PresioneENTER();
+            notastring = Console.ReadLine();
+
+
+            try
+            {
+                newEval.Nota = float.Parse(notastring);
+                if (newEval.Nota < 0 || newEval.Nota > 5)
+                {
+                    throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
+                }
+                WriteLine("La nota de la evaluacion ha sido ingresada correctamente");
+                return;
+            }
+            catch (ArgumentOutOfRangeException arge)
+            {
+                Printer.WriteTitle(arge.Message);
+                WriteLine("Saliendo del programa");
+            }
+            finally
+            {
+                Printer.WriteTitle("FINALLY");
+                Printer.Beep(2500, 500, 3);
+
+            }
+            // catch(Exception)
+            // {
+            //     Printer.WriteTitle("El valor de la nota no es un número válido");
+            //     WriteLine("Saliendo del programa");
+
 
         }
 
